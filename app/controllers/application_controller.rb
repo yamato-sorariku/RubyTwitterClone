63329c4562d474388bf3a1edb3ed46f067edc4de
Yamato Ashikaga <a.yamato.so.ri.00@gmail.com>
2021-04-28 00:04:52 +0900

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::API
     authenticate_or_request_with_http_token do |token|
       @auth_user = User.find_by(token: token)
       @auth_user != nil ? true : false
+      @auth_user != nil ? true : false
     end
   end
 end
